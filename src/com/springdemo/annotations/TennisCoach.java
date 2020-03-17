cb4992a057efe6a99469f37883a6af444bc0c45e
isehead <isehead@gmail.com>
2020-03-17 15:07:20 +0300

    @Autowired: field injection

--- src/com/springdemo/annotations/TennisCoach.java
+++ src/com/springdemo/annotations/TennisCoach.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 
+    @Autowired
     private FortuneService fortuneService;
 
     public TennisCoach(){
@@ -17,11 +18,11 @@ public class TennisCoach implements Coach {
         fortuneService = theFortuneService;
     }*/
 
-    @Autowired
-    public void setFortuneService(FortuneService fortuneService) {
-        System.out.println("TennisCoach - inside the setter");
+/*    @Autowired
+    public void doSomeStuff(FortuneService fortuneService) {
+        System.out.println("TennisCoach - inside the doSomeStuff method");
         this.fortuneService = fortuneService;
-    }
+    }*/
 
     @Override
     public String getDailyWorkout() {
