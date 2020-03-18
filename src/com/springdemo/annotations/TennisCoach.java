5e73edd27970c31d98aaf4ffa4d12fee25ed431a
isehead <isehead@gmail.com>
2020-03-18 21:11:14 +0300

    PostConstruct and PreDestroy

--- src/com/springdemo/annotations/TennisCoach.java
+++ src/com/springdemo/annotations/TennisCoach.java
@@ -2,7 +2,11 @@ package com.springdemo.annotations;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @Component
 public class TennisCoach implements Coach {
@@ -14,6 +18,19 @@ public class TennisCoach implements Coach {
     public TennisCoach(){
         System.out.println("TennisCoach - inside the default constructor");
     };
+
+    // define init-method
+    @PostConstruct
+    public void doStartup(){
+        System.out.println("Doing startup stuff");
+    }
+
+    // define destroy-method
+    @PreDestroy
+    public void doCleanup(){
+        System.out.println("Doing cleanup stuff");
+    }
+
 
 /*    @Autowired
     public TennisCoach(FortuneService theFortuneService) {
