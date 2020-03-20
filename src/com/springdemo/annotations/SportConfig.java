05f77ac0dfc576a9d8d08125d9d04c12d82b6353
isehead <isehead@gmail.com>
2020-03-20 22:24:28 +0300

    Injecting values from .properties file

--- src/com/springdemo/annotations/SportConfig.java
+++ src/com/springdemo/annotations/SportConfig.java
@@ -3,8 +3,10 @@ package com.springdemo.annotations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:sport2.properties")
 //@ComponentScan("com.springdemo.annotations")
 public class SportConfig {
 
--- src/com/springdemo/annotations/SwimJavaConfigDemoApp.java
+++ src/com/springdemo/annotations/SwimJavaConfigDemoApp.java
@@ -11,13 +11,19 @@ public class SwimJavaConfigDemoApp {
                 new AnnotationConfigApplicationContext(SportConfig.class);
 
         // get the bean from Spring container
-        Coach theCoach = applicationContext.getBean("swimCoach", Coach.class);
+        // duplicated to inject values from properties file
+        // Coach theCoach = applicationContext.getBean("swimCoach", Coach.class);
+        SwimCoach theCoach = applicationContext.getBean("swimCoach", SwimCoach.class);
 
         // call methods on the bean
         System.out.println(theCoach.getDailyWorkout());
 
         // call a method to get daily fortune
         System.out.println(theCoach.getDailyFortune());
+
+        // call new SwimCoach methods with properties injected
+        System.out.println("Email: " + theCoach.getEmail());
+        System.out.println("Team: " + theCoach.getTeam());
 
         // close the context
         applicationContext.close();
