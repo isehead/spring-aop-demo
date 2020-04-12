1862f3bab453280e7eb6f359551b0092b2111833
isehead <isehead@gmail.com>
2020-04-12 22:28:57 +0500

    First custom @PointCut created

--- src/com/spring/aopdemo/aspect/DemoLoggingAspect.java
+++ src/com/spring/aopdemo/aspect/DemoLoggingAspect.java
@@ -2,6 +2,7 @@ package com.spring.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -12,10 +13,17 @@ public class DemoLoggingAspect {
 
     // start with @Before advice testing
 
-//    @Before("execution(public void add*())")
+    @Pointcut("execution(* com.spring.aopdemo.dao.*.*(..))")
+    private void forDaoPackage(){}
 
-    @Before("execution(* com.spring.aopdemo.dao.*.*(..))")
+//    @Before("execution(public void add*())")
+    @Before("forDaoPackage()")
     public void beforeAddAccountAdvice(){
         System.out.println("\n=====>>> Executing @Before advice on addAccount()");
+    }
+
+    @Before("forDaoPackage()")
+    public void performApiAnalytics(){
+        System.out.println("========= API ANALYTICS =========");
     }
 }
